e3f966916d952869b74977a1c0e3f5e7d7960d9f
Harshita1204 <harshitak1204@gmail.com>
2025-12-29 15:39:11 +0530

    Add Fibonacci series program in C++
    Implement Fibonacci series calculation and output

---
+++ fibonacci_series.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"Enter the value of n: ";
+   cin>>n;
+  int a=0;
+  int b=1;
+  cout<<a<<" "<<b<<" ";
+  
+  for(int i=1; i<=n; i++){
+      int nextNumber=a+b;
+      cout<<nextNumber<<" ";
+      //first transfer the value of b into a then nextNumber to b 
+      a=b;
+      b=nextNumber;
+  }
+   return 0;
+}
